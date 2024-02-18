51a5de91a5ae53ed29fd7fd43bec31499170fe6e
k2Forest-Man <LesnoiChelovek@yandex.ru>
2024-02-18 23:01:28 +0300

    logic for displaying buttons

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const formFieldsets = document.querySelectorAll('.form__fieldset');
 const formBtnPrev = document.querySelector('.form__btn-prev');
 const formBtnNext = document.querySelector('.form__btn-next');
 const formBtnSubmit = document.querySelector('.form__btn-submit');
+const formTime = document.querySelector('.form__time');
 
 let currentStep = 0;
 
@@ -30,18 +31,56 @@ const updateFieldsetVisibility = () => {
   }
 };
 
-formBtnNext.addEventListener('click', () => {
-  if (currentStep < formFieldsets.length - 1) {
-    currentStep += 1;
-    updateFieldsetVisibility();
-  }
-});
+const createFormTime = () => {
+  formTime.style.display = 'block';
+}
 
-formBtnPrev.addEventListener('click', () => {
-  if (currentStep > 0) {
-    currentStep -= 1;
-    updateFieldsetVisibility();
-  }
-});
+// При взаимодействии с инпутами в форме, разблокируем кнопку
+const handleInputForm = ({ currentTarget, target }) => {
+  if (currentTarget.type.value && currentStep === 0) {
+    formBtnNext.disabled = false;
 
-updateFieldsetVisibility();
+    //createFormMonth()
+    //createFormDay()
+  }
+
+  if (currentStep === 1) {
+    if (currentTarget.day.value && target.name === 'day') {
+
+      createFormTime()
+    }
+
+    if (currentTarget.day.value && currentTarget.time.value && target.name === 'time') {
+      formBtnNext.disabled = false;
+    } else {
+      formBtnNext.disabled = true;
+    }
+  }
+}
+
+const init = () => {
+  formBtnNext.disabled = true;
+
+  formBtnNext.addEventListener('click', () => {
+    if (currentStep < formFieldsets.length - 1) {
+      currentStep += 1;
+      updateFieldsetVisibility();
+      formBtnNext.disabled = true;
+      formBtnSubmit.disabled = true;
+    }
+  });
+  
+  formBtnPrev.addEventListener('click', () => {
+    if (currentStep > 0) {
+      currentStep -= 1;
+      updateFieldsetVisibility();
+      formBtnNext.disabled = false;
+    }
+  });
+  
+  form.addEventListener('input', handleInputForm)
+  
+  updateFieldsetVisibility();
+};
+
+init();
